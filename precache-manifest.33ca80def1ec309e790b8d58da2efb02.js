390a9d07d49884704ce0066014eaf9c6e5acc362
Fredericia <39918560+stevD9@users.noreply.github.com>
2019-08-29 20:59:50 +0300

    Updates

---
+++ precache-manifest.33ca80def1ec309e790b8d58da2efb02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a806f0a5af5d12b6c96924acef7c34a8",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7632514e8ccf6794f73e",
+    "url": "/react-tic-tac-toe/static/css/main.17f5b708.chunk.css"
+  },
+  {
+    "revision": "5acaf3b9de72c8f2bb9f",
+    "url": "/react-tic-tac-toe/static/js/2.18dce7a4.chunk.js"
+  },
+  {
+    "revision": "7632514e8ccf6794f73e",
+    "url": "/react-tic-tac-toe/static/js/main.f67a050f.chunk.js"
+  },
+  {
+    "revision": "3f6fe887e5a1140dc573",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.310a450b.js"
+  }
+]);
